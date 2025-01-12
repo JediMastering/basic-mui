39eb7accda79606414d360a9bf55caf2d61843a4
Ale <jedimasteringale@gmail.com>
2025-01-11 22:46:34 -0300

    Criando estrutura de proteção de rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Login from "./pages/Login";
-import NotFound from "./components/NotFound";
-import System from "./layout/System"
-import Settings from "./pages/Settings";
-import Profile from "./pages/Profile";
-import Dashboard from "./pages/Dashboard";
-import Test from "./pages/Test";
+import ProtectedRoute from "./service/ProtectedRoute";
+import routes from "./service/Routes";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<System><Home /></System>} />
-        <Route path="test" element={<Test />} />
-        <Route path="/dashboard" element={<System><Dashboard /></System>} />
-        <Route path="/settings" element={<System><Settings /></System>} />
-        <Route path="/profile" element={<System><Profile /></System>} />
-        <Route path="*" element={<NotFound />} />
+        {routes.map(({ path, element, isProtected }, index) => (
+          <Route
+            key={index}
+            path={path}
+            element={
+              isProtected ? (
+                <ProtectedRoute>{element}</ProtectedRoute>
+              ) : (
+                element
+              )
+            }
+          />
+        ))}
       </Routes>
     </Router>
   );
---
+++ src/service/Routes.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Home from "../components/Home";
+import Login from "../pages/Login";
+import NotFound from "../components/NotFound";
+import System from "../layout/System";
+import Settings from "../pages/Settings";
+import Profile from "../pages/Profile";
+import Dashboard from "../pages/Dashboard";
+import Test from "../pages/Test";
+
+const routes = [
+  { path: "/", element: <Login />, isProtected: false },
+  { path: "/home", element: <System><Home /></System>, isProtected: true },
+  { path: "/dashboard", element: <System><Dashboard /></System>, isProtected: true },
+  { path: "/settings", element: <System><Settings /></System>, isProtected: true },
+  { path: "/profile", element: <System><Profile /></System>, isProtected: true },
+  { path: "/test", element: <Test />, isProtected: false },
+  { path: "*", element: <NotFound />, isProtected: false },
+];
+
+export default routes;
