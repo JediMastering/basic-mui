3cbbc3d5b1020e59c0f4e91ea456452a575da10d
Ale <jedimasteringale@gmail.com>
2025-01-12 18:46:14 -0300

    Configurando MUI para pt-br

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { ptBR } from "@mui/material/locale";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import ProtectedRoute from "./service/ProtectedRoute";
 import routes from "./service/Routes";
 
+// Cria o tema com suporte ao idioma português
+const theme = createTheme({}, ptBR);
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        {routes.map(({ path, element, isProtected }, index) => (
-          <Route
-            key={index}
-            path={path}
-            element={
-              isProtected ? (
-                <ProtectedRoute>{element}</ProtectedRoute>
-              ) : (
-                element
-              )
-            }
-          />
-        ))}
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
+        <CssBaseline />
+        <Router>
+          <Routes>
+            {routes.map(({ path, element, isProtected }, index) => (
+              <Route
+                key={index}
+                path={path}
+                element={
+                  isProtected ? (
+                    <ProtectedRoute>{element}</ProtectedRoute>
+                  ) : (
+                    element
+                  )
+                }
+              />
+            ))}
+          </Routes>
+        </Router>
+      </LocalizationProvider>
+    </ThemeProvider>
   );
 };
 
