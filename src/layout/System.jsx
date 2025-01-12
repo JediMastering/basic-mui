db70d580853c07156bcfb8997f9a3b430f6a1ace
Ale <jedimasteringale@gmail.com>
2025-01-12 15:23:36 -0300

    Adicionando handle ao fazer logout

--- src/layout/System.jsx
+++ src/layout/System.jsx
@@ -1,48 +1,42 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import MenuIcon from "@mui/icons-material/Menu";
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
+import React from "react";
+import Box from "@mui/material/Box";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import IconButton from "@mui/material/IconButton";
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import MailIcon from '@mui/icons-material/Mail';
-import { AppBar, Toolbar } from '@mui/material';
-import { blueGrey } from '@mui/material/colors';
-import { useTheme } from '@mui/material/styles';
-import { Container } from "@mui/material";
-
-import { Link } from "react-router-dom";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { AppBar, Toolbar, Container, ListItemButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { blueGrey } from "@mui/material/colors";
+import { useNavigate, Link } from "react-router-dom";
 
 const blueGreyColor = blueGrey[800];
 
 export default function SwipeableTemporaryDrawer({ children }) {
   const [state, setState] = React.useState({ left: false });
-  const theme = useTheme();
+  const navigate = useNavigate();
 
   // Função para abrir/fechar o Drawer
   const toggleDrawer = (anchor, open) => (event) => {
     if (
       event &&
-      event.type === 'keydown' &&
-      (event.key === 'Tab' || event.key === 'Shift')
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
     ) {
       return;
     }
     setState({ ...state, [anchor]: open });
   };
 
+  // Função de Logout
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/"); // Redireciona para a página inicial
+  };
+
   // Lista de itens do Drawer
   const list = (anchor) => (
     <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
@@ -57,16 +51,21 @@ export default function SwipeableTemporaryDrawer({ children }) {
         <ListItem component={Link} to="/profile">
           <ListItemText primary="Perfil" />
         </ListItem>
-        <ListItem component={Link} to="/">
+        {/* Botão de Logout */}
+        <ListItemButton
+          onClick={handleLogout}
+          component="a"
+          sx={{color: "rgba(117, 117, 117, 0.87)"}}
+        >
           <ListItemText primary="Logout" />
-        </ListItem>
+        </ListItemButton>
       </Box>
     </Box>
   );
 
   return (
     <div>
-      {['left'].map((anchor) => (
+      {["left"].map((anchor) => (
         <React.Fragment key={anchor}>
           {/* AppBar Fixado no topo */}
           <AppBar position="fixed" sx={{ backgroundColor: blueGreyColor }}>
@@ -93,20 +92,13 @@ export default function SwipeableTemporaryDrawer({ children }) {
             {list(anchor)}
           </SwipeableDrawer>
 
-          {/* Container sem margem à esquerda, alinhado ao topo da página */}
+          {/* Container para conteúdo principal */}
           <Container
             sx={{
               marginTop: 8, // Ajuste a margem superior conforme necessário
               paddingTop: 1,
-              paddingLeft: 0, // Remover o padding das laterais
-              paddingRight: 0, // Remover o padding das laterais
-              '@media (min-width:600px)': {
-                paddingLeft: 0, // Definir o padding para telas maiores que 600px
-                paddingRight: 0, // Definir o padding para telas maiores que 600px
-              },
             }}
           >
-            {/* Conteúdo aqui */}
             {children}
           </Container>
         </React.Fragment>
